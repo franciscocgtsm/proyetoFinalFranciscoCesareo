c75964479ee27bde56f1fa5f2d9622af917d5dec
franciscocgtsm <93315935+franciscocgtsm@users.noreply.github.com>
2025-08-08 10:49:44 +0200

    Delete tienda directory

--- tienda/js/cookies.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    if (!document.cookie.includes("visita")) {
-        alert("Bienvenido a la Tienda Online");
-        let fecha = new Date();
-        fecha.setTime(fecha.getTime() + (24 * 60 * 60 * 1000)); // 1 día
-        document.cookie = "visita=1; expires=" + fecha.toUTCString() + "; path=/";
-    }
-});
